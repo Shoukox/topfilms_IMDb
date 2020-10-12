f938850ca50242ffe02a56b2a40e480a51a865b9
Shoukox <58671746+Shoukox@users.noreply.github.com>
2020-10-12 14:28:12 +0500

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace topimdb
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.textBoxFilms = new System.Windows.Forms.TextBox();
+            this.checkBoxRandom = new System.Windows.Forms.CheckBox();
+            this.textBoxFilters = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Font = new System.Drawing.Font("Segoe UI Semibold", 15F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.buttonStart.Location = new System.Drawing.Point(407, 354);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(113, 48);
+            this.buttonStart.TabIndex = 1;
+            this.buttonStart.Text = "Start";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // textBoxFilms
+            // 
+            this.textBoxFilms.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.textBoxFilms.Location = new System.Drawing.Point(12, 12);
+            this.textBoxFilms.Multiline = true;
+            this.textBoxFilms.Name = "textBoxFilms";
+            this.textBoxFilms.ReadOnly = true;
+            this.textBoxFilms.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxFilms.Size = new System.Drawing.Size(508, 336);
+            this.textBoxFilms.TabIndex = 2;
+            this.textBoxFilms.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // checkBoxRandom
+            // 
+            this.checkBoxRandom.AutoSize = true;
+            this.checkBoxRandom.Location = new System.Drawing.Point(293, 354);
+            this.checkBoxRandom.Name = "checkBoxRandom";
+            this.checkBoxRandom.Size = new System.Drawing.Size(108, 17);
+            this.checkBoxRandom.TabIndex = 3;
+            this.checkBoxRandom.Text = "Рандом фильм?";
+            this.checkBoxRandom.UseVisualStyleBackColor = true;
+            // 
+            // textBoxFilters
+            // 
+            this.textBoxFilters.Location = new System.Drawing.Point(143, 354);
+            this.textBoxFilters.Multiline = true;
+            this.textBoxFilters.Name = "textBoxFilters";
+            this.textBoxFilters.Size = new System.Drawing.Size(132, 48);
+            this.textBoxFilters.TabIndex = 4;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(293, 377);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(104, 30);
+            this.checkBox1.TabIndex = 5;
+            this.checkBox1.Text = "Сортировать \r\nпо жанрам?";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(526, 414);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.textBoxFilters);
+            this.Controls.Add(this.checkBoxRandom);
+            this.Controls.Add(this.textBoxFilms);
+            this.Controls.Add(this.buttonStart);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.TextBox textBoxFilms;
+        private System.Windows.Forms.CheckBox checkBoxRandom;
+        private System.Windows.Forms.TextBox textBoxFilters;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Net.Http;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+using AngleSharp;
+
+
+namespace topimdb
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        async Task<string> LoadData()
+        {
+            try
+            {
+                string response = null;
+                List<string> data = new List<string>();
+                using (HttpClient hc = new HttpClient())
+                {
+                    HttpResponseMessage hrm = await hc.GetAsync("https://ru.wikipedia.org/wiki/250_%D0%BB%D1%83%D1%87%D1%88%D0%B8%D1%85_%D1%84%D0%B8%D0%BB%D1%8C%D0%BC%D0%BE%D0%B2_%D0%BF%D0%BE_%D0%B2%D0%B5%D1%80%D1%81%D0%B8%D0%B8_IMDb");
+                    hrm.EnsureSuccessStatusCode();
+                    response = await hrm.Content.ReadAsStringAsync();
+                    textBoxFilms.Text = response;
+                }
+                using (BrowsingContext bc = new BrowsingContext(Configuration.Default))
+                {
+                    var document = await bc.OpenAsync(m => m.Content(response));
+                    var list = document.QuerySelectorAll("tr").Skip(2);
+                    foreach (var item in list)
+                    {
+                        var temp = item.QuerySelectorAll("td");
+                        data.Add($"{temp[0].TextContent}. {temp[1].TextContent}\r\n   Год выпуска: {temp[2].TextContent}\r\n   Режиссер: {temp[3].TextContent}\r\n   Жанры: {temp[4].TextContent}\r\n\r\n");
+                    }
+                    if (!checkBoxRandom.Checked && !checkBox1.Checked)
+                    {
+                        string text = "";
+                        foreach (var item in data)
+                        {
+                            text += item;
+                        }
+                        return $"Рейтинг фильмов по версии IMDb\r\n{document.QuerySelectorAll("span").First(m => m.ClassName == "mw-headline" && m.TextContent.StartsWith("Состояние")).TextContent}:\r\n{text}";
+                    }
+                    else if (checkBoxRandom.Checked)
+                        return $"Случайный фильм:\r\n\r\n{data[new Random().Next(0, 250)]}";
+                    else if (checkBox1.Checked)
+                    {
+                        var ab = textBoxFilters.Text.Split("-".ToCharArray());
+                        bool contains = false;
+                        string text = "";
+                        foreach(var item in data)
+                        {
+                            contains = false;
+                            foreach(var item1 in ab)
+                            {
+                                if (item.Contains(item1))
+                                {
+                                    contains = true;
+                                    break;
+                                }
+                            }
+                            if (contains) text += item;
+                        }
+                        return $"Фильтр: {string.Join(", ", ab)}\r\n\r\n{text}";
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show($"Попробуйте еще раз!\nОшибка: {e.Message}");
+            }
+            return null;
+        }
+
+        async private void buttonStart_Click(object sender, EventArgs e)
+        {
+            textBoxFilms.Text = await LoadData();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                MessageBox.Show("Вводите текст в поле левее через дефис (драма-Крестный-Джордж). По этому тексту будут отсортированы фильмы (и по жанрам, и по режиссеру, и по названию)");
+            }
+        }
+    }
+}
